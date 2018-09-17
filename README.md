941d3b2d2e27dd2b1d1cf26c1fbe0066cfda8637
rohitrastogi <rohitrastogi2019@u.northwestern.edu>
2018-09-16 22:26:39 -0500

    improved parallel exhaustive enumeration speed to 22 seconds by chunking and reducing gets

--- README.md
+++ README.md
@@ -7,15 +7,16 @@ todos:
 
 
 Findings so far:
-input: ([[(13, 'D'), (13, 'H')],[(2, 'S'), (7, 'H')]], []) -> (Aces vs 2/7 - 87% in favor of aces)
+- input: ([[(13, 'D'), (13, 'H')],[(2, 'S'), (7, 'H')]], []) -> (Aces vs 2/7 - 87% in favor of aces)
 - monte-carlo time: .36 seconds
 - exhaustive enumeration: ~ 40.75 seconds (main thread)
 - parallelized exhaustive enumeration (w queue implementation) : 70.96 seconds (4 threads)
 
 why parallel version is slow?
-- cannot map workers because random board function is a generator
+- cannot map workers because exhaustive enumeration board function is a generator (don't want 48C5 boards stored in an array in memory)
 - need to add board to a thread safe queue (producer/consumer)
-- queue.get() is as slow as 0.002340253002330428 seconds. So in a sense, parallel.py is IO/bound 
+- first thought: maybe queue.put() is slower than queue.get()? Doesn't seem this way - generated boards aren't big enough to be buffered 
+- ISSUE: queue.get() is as slow as 0.002340253002330428 seconds. So in a sense, parallel.py is IO/bound 
 
 potential solutions:
 - use pipes
--- src/parallel.py
+++ src/parallel.py
@@ -4,19 +4,34 @@ import constants
 
 #TODO: investigate performance of using pipes instead of queues
 
-def process(input_queue, output_queue, hole_cards):
+def chunk_generator(generator, chunk_size = 1000):
+    while True:
+        res = None
+        temp = []
+        for _ in range(chunk_size):
+            try: 
+                temp.append(next(generator))
+            except:
+                yield temp
+                raise StopIteration
+        res = temp
+        temp = []
+        yield res 
+
+def process(input_queue, output_queue, hole_cards, chunks):
     hand_hists = [[0]*len(constants.HANDS) for hand in hole_cards]
     win_hist = [0] * (len(hole_cards) + 1)
     i = 0
     while True:
         i += 1
-        curr = utils.timeit.default_timer()
-        board = input_queue.get()
-        print(utils.timeit.default_timer() - curr)
-        if not board:
+        boards = input_queue.get()
+        if not boards:
             break
-        utils.update_simulation_state(hole_cards, board, hand_hists, win_hist)
-    print(i)
+        if not chunks:
+            utils.update_simulation_state(hole_cards, boards, hand_hists, win_hist)
+        else:
+            for board in boards:
+                utils.update_simulation_state(hole_cards, board, hand_hists, win_hist)
     output_queue.put((hand_hists, win_hist))
 
 def reduce_process_results(queue):
@@ -34,17 +49,20 @@ def reduce_process_results(queue):
         return hand_hists_sum, win_hist_sum
     return reduce(helper, queue_list)
 
-def run_simulation_parallel(hole_cards, board):
+def run_simulation_parallel(hole_cards, board, chunks = False):
     start_time = utils.timeit.default_timer()
     deck = utils.generate_deck(hole_cards, board)
-    #TODO: experiment with input_queue size relative to the number of processes
-    #discover ratio of producer(generator)/consumer(equity calculation)
     input_queue = mp.Queue(maxsize = 100)
     output_queue = mp.Queue(maxsize = 4)
-    pool = mp.Pool(4, initializer = process, initargs = (input_queue, output_queue, hole_cards))
-    for board in utils.enumerate_boards(deck, len(board)):
-        input_queue.put(board)
-    for _ in range(1):
+    pool = mp.Pool(4, initializer = process, initargs = (input_queue, output_queue, hole_cards, chunks))
+    if not chunks:
+        for board in utils.enumerate_boards(deck, len(board)):
+            input_queue.put(board)
+    else:
+        generator = utils.enumerate_boards(deck, len(board))
+        for boards in chunk_generator(generator, 1000):
+            input_queue.put(boards)
+    for _ in range(4):
         input_queue.put(None)
     pool.close()
     pool.join()
@@ -56,5 +74,4 @@ def run_simulation_parallel(hole_cards, board):
     print(hand_perc)
     
 #should be roughly 87% in favor of aces
-run_simulation_parallel([[(13, 'D'), (13, 'H')],[(2, 'S'), (7, 'H')]], [])
-
+run_simulation_parallel([[(13, 'D'), (13, 'H')],[(2, 'S'), (7, 'H')]], [], True)
